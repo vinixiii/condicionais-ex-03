91978a11bd128ee14c824f6d95f36594f1a5faeb
Vinícius <vinissfigueiroa@gmail.com>
2020-11-15 18:31:35 -0300

    Exercício 03 - Feito

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercise_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício - 03");
+            Console.WriteLine("----------");
+            Console.WriteLine("");
+            
+            Console.WriteLine("Digite o salário do funcionário:");
+            double salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            if(salario >= 500.00) {
+                double aumento = salario * 0.3;
+                double novoSalario = salario + aumento;
+
+                Console.WriteLine("O salário deste funcionário será reajustado para: " + novoSalario + " reais.");
+            } else {
+                Console.WriteLine("O salári deste funcionário não será reajustado.");
+            }
+        }
+    }
+}
